3b68d96bf7635c46dcbfa05176f2d341b1fa6243
Shodgearo <sanya.93.26@bk.ru>
2019-12-25 13:24:45 +0600

    Add dynamic update root view

--- src/main/java/com/quiz/controllers/HomeController.java
+++ src/main/java/com/quiz/controllers/HomeController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.Map;
+
 @Controller
 public class HomeController {
     @Autowired
@@ -22,10 +24,13 @@ public class HomeController {
     }
 
     @PostMapping("/")
-    public String add(@RequestParam String name, @RequestParam String text, @RequestParam String step, @RequestParam int score, Model model) {
+    public String add(@RequestParam String name, @RequestParam String text, @RequestParam String step, @RequestParam int score, Map<String, Object> model) {
         Question question = new Question(name, text, step, score);
 
         questionRepo.save(question);
+
+        // Чтобы после отправки формы сразу в листе был добавленный вопрос
+        model.put("questions", questionRepo.findAll());
 
         return "home";
     }
